224106f8a4138cc1fd897bf183cdde685cd5c4fb
Sergei <litrax@bk.ru>
2023-04-19 00:22:36 +0300

    I hope...

---
+++ webapp/models.py
@@ -0,0 +1,39 @@
+import datetime
+
+from flask_login import UserMixin
+
+from webapp import db, manager
+
+
+class Feedback(db.Model):
+    __tablename__ = 'feedback'
+    id = db.Column(db.Integer, primary_key=True)
+    text = db.Column(db.String(1024), nullable=False)
+    date = db.Column(db.DateTime, default=datetime.datetime.utcnow())
+
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+    user = db.relationship('User',
+                           backref=db.backref('feedbacks', lazy='joined'),
+                           lazy=True)
+
+    def __repr__(self):
+        return '<Feedback %r>' % self.text
+
+
+class User(db.Model, UserMixin):
+    __tablename__ = 'user'
+    id = db.Column(db.Integer, primary_key=True)
+    login = db.Column(db.String(128),
+                      nullable=False, unique=True)
+    password = db.Column(db.String(255), nullable=False)
+
+    def __repr__(self):
+        return '<User %r>' % self.id
+
+
+
+
+
+@manager.user_loader
+def load_user(user_id):
+    return User.query.get(user_id)
---
+++ webapp/routes.py
@@ -0,0 +1,108 @@
+from flask import request, redirect, render_template, url_for, flash
+from flask_login import login_user, login_required, logout_user
+from werkzeug.security import check_password_hash, generate_password_hash
+
+from webapp import app, db
+from webapp.models import Feedback, User
+
+
+personal_log = 'qwe'
+
+
+@app.route('/update', methods=['GET'])
+@app.route('/', methods=['GET'])
+def updates_page():
+    with open('webapp/static/text/updates.txt', 'r', encoding="utf-8") as f:
+        updates_data = f.readlines()[0]
+    return render_template('index.html', updates=updates_data)
+
+
+@app.route('/feedbacks', methods=['GET'])
+def feedbacks_page():
+    return render_template('feedbacks.html',
+                           items=User.query.filter(Feedback.id > 0).all())
+
+
+@app.route('/feedback_form', methods=['GET', 'POST'])
+@login_required
+def feedback_form_page():
+    edit = False
+    user = User.query.filter(User.login == personal_log).first()
+    try:
+        if user.feedbacks:
+            edit = True
+    except Exception:
+        pass
+
+    if request.method == 'POST':
+        text = request.form['text']
+        if edit:
+            user.feedbacks[0].text = text
+        else:
+            feedback = Feedback(text=text, user=user)
+            user.feedbacks.append(feedback)
+        db.session.commit()
+        return redirect(url_for('feedbacks_page'))
+    else:
+        return render_template('feedback_form.html', edit=edit)
+
+
+@app.route('/login', methods=['GET', 'POST'])
+def login_page():
+    login = request.form.get('login')
+    password = request.form.get('password')
+
+    if login and password:
+        user = User.query.filter_by(login=login).first()
+        if user and check_password_hash(user.password, password):
+            login_user(user)
+
+            #  next_page = request.args.get('next')
+
+            global personal_log
+            personal_log = login
+
+            redirect(url_for('feedback_form_page'))
+        else:
+            flash("login or password not correct")
+    else:
+        flash("please fill login and password")
+    return render_template('login.html')
+
+
+@app.route('/register', methods=['GET', 'POST'])
+def register_page():
+    login = request.form.get('login')
+    password = request.form.get('password')
+    password2 = request.form.get('password2')
+
+    if request.method == 'POST':
+        if not (login or password or password2):
+            flash('Заполни меня')
+        elif password != password2:
+            flash('Пароли не равны')
+        else:
+            hash_psw = generate_password_hash(password)
+            new_user = User(login=login)
+            new_user.password = hash_psw
+            db.session.add(new_user)
+            db.session.commit()
+            login_user(new_user)
+            return redirect(url_for('feedback_form_page'))
+
+        return redirect(url_for('login_page'))
+    return render_template('register.html')
+
+
+@app.route('/logout', methods=['GET', 'POST'])
+@login_required
+def logout_page():
+    logout_user()
+    return redirect(url_for('feedbacks_page'))
+
+
+@app.after_request
+def redirect_to_signin(response):
+    if response.status_code == 401:
+        return redirect(url_for('login_page'))
+    return response
